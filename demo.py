4e29b4bd593865379def690f141f6223bb4fd812
jhan53 <jhan53@jhu.edu>
2018-08-12 00:49:30 -0400

    update

--- demo.py
+++ demo.py
@@ -18,12 +18,12 @@ from trajectory_msgs.msg import *
 from sensor_msgs.msg import JointState
 import actionlib
 import rospy
-from ros_myo.msg import EmgArray
 from std_msgs.msg import String, Header
 from geometry_msgs.msg import WrenchStamped, Vector3
 import tf
 from tf.transformations import *
-from icl_phri_robotiq_control.robotiq_utils import *
+from math import pi
+# from icl_phri_robotiq_control.robotiq_utils import *
 from inverseKinematicsUR5 import InverseKinematicsUR5, transformRobotParameter
 
 
@@ -34,37 +34,43 @@ rad_to_ang = lambda x: x / np.pi * 180.
 ang_to_rad = lambda x: x / 180. * np.pi
 JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
                'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
-Q1 = [0.09981511533260345, -2.024665657673971, -0.7203519980060022, -1.7111480871783655, 1.5250813961029053, 0.0007902098586782813]
-Q2 = [0.09786205738782883, -2.2906211058246058, -0.7209880987750452, -1.749324146901266, 1.5192002058029175, 0.001340735238045454]
-Q3 = [-1.725508991871969, -0.9326947371112269, -1.830911938344137, -0.3809617201434534, 1.7116585969924927, 0]
 
 
-class Handover:
+class IK_solver:
     def __init__(self):
         #/vel_based_pos_traj_controller/
-        self.client = actionlib.SimpleActionClient('icl_phri_ur5/follow_joint_trajectory', FollowJointTrajectoryAction)
+        # icl_phri_ur5
+        self.client = actionlib.SimpleActionClient('arm_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
         self.goal = FollowJointTrajectoryGoal()
         self.goal.trajectory = JointTrajectory()
         self.goal.trajectory.joint_names = JOINT_NAMES
         print ("Waiting for server...")
         self.client.wait_for_server()
         print ("Connected to server")
-        joint_states = rospy.wait_for_message("icl_phri_ur5/joint_states", JointState)
+        joint_states = rospy.wait_for_message("joint_states", JointState)
         self.joints_pos_start = np.array(joint_states.position)
         print ("Init done")
-        self.listener = tf.TransformListener()
+        # self.listener = tf.TransformListener()
+        # self.Transformer = tf.TransformerROS()
 
-    def move(self):
+        joint_weights = [6,5,4,3,2,1]
+        self.ik = InverseKinematicsUR5()
+        self.ik.setJointWeights(joint_weights)
+        #self.ik.setJointLimits(-pi, pi)
+
+
+    def move(self, dest_m):
         current_m = transformRobotParameter(self.joints_pos_start)
-        hand_base = self._get_transform('/ee_link', '/right_hand_1')
-        gripper_hand = None
-        Desti_m = self._cal_dest(gripper_hand, hand_base)
-        sols = inverseinverse(np.array(Desti_m), float(self.joints_pos_start[5]))
-        qsol = best_sol(sols, self.joints_pos_start, [1.]*6)
+        qsol = self.ik.findClosestIK(dest_m,self.joints_pos_start)
+        #qsol[0] -= pi
+        if qsol[0] < 0:
+            qsol[0] += pi
+        else:
+            qsol[0] -= pi
         if qsol is not None:
             self.goal.trajectory.points = [
                 JointTrajectoryPoint(positions=self.joints_pos_start.tolist(), velocities=[0]*6, time_from_start=rospy.Duration(0.0)),
-                JointTrajectoryPoint(positions=qsol.tolist(), velocities=[0]*6, time_from_start=rospy.Duration(1./125.)),
+                JointTrajectoryPoint(positions=qsol.tolist(), velocities=[0]*6, time_from_start=rospy.Duration(1)),
             ]
             print('start: ' + str(self.joints_pos_start.tolist()))
             print('goal: ' + str(qsol.tolist()))
@@ -73,22 +79,6 @@ class Handover:
                 self.joints_pos_start = qsol
             except:
                 raise
-    
-    def _get_transform(self, target_frame, source_frame, is_matrix=True):
-        rate = rospy.Rate(10.0)
-        rate.sleep()        
-        # try:
-        (trans, quat) = self.listener.lookupTransform(target_frame, source_frame, rospy.Time(0))
-        if is_matrix:
-            rot = euler_from_quaternion(quat)            
-            return np.asarray(tf.TransformerROS.fromTranslationRotation(trans, rot))
-        else:
-            return trans, quat
-        # except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        #     print("wait for tf")
-    
-    def _cal_dest(self, gripper_hand, hand_base):
-        return np.dot(gripper_hand , hand_base)
         
 
 
@@ -99,19 +89,21 @@ class Handover:
 
 if __name__ == '__main__':
     rospy.init_node('test', anonymous=True)
-    h = Handover()
-    rospy.sleep(1)
-    while not rospy.is_shutdown():
-        try:
-            # rospy.sleep(1)
-            trans = h._get_transform('/left_hand_1', '/right_hand_1')[0]
-            # print("hand_gripper_tf_trans:{}\nhand_gripper_tf_quat:{}".format(
-            #     h._get_transform('/ee_link', '/right_hand_1')[0],
-            #     h._get_transform('/ee_link', '/right_hand_1')[1]
-            # ))
-            # print(trans)
-            h.frame_action(trans)
-        except e:
-            print(e)
+    h = IK_solver()
+    listener = tf.TransformListener()
+    Transformer = tf.TransformerROS()
+    try:
+        listener.waitForTransform('/base_link','/target',rospy.Time(), rospy.Duration(4.0))
+        position, quaternion = listener.lookupTransform('/base_link', '/target',rospy.Time(0))
+        transform = Transformer.fromTranslationRotation(position, quaternion) 
+        h.move(transform)
+    except Exception as e:
+        print(e)
+    rospy.spin()
+    # while not rospy.is_shutdown():
+    #     try:
+            
+    #     except e:
+    #         print(e)
 
 
--- test_ik.py
+++ test_ik.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 from inverseKinematicsUR5 import InverseKinematicsUR5, transformRobotParameter
 import numpy as np
 from math import pi
-
+from sensor_msgs.msg import JointState
 import rospy
 import tf
 
@@ -19,8 +20,10 @@ class ur5_joint_estimator:
 	def estimateJointValueByUsingTF(self):
 		position, quaternion = self.tf_listener.lookupTransform(self.base_link, self.ee_link,rospy.Time(0))
 		transform = self.Transformer.fromTranslationRotation(position, quaternion)
-		print 'input transform:\n', transform
+		#print 'input transform:\n', transform
+		joint_states = rospy.wait_for_message("joint_states", JointState)
 		print 'Solutions:\n', self.ik.solveIK(transform)
+		print joint_states.position
 
 rospy.init_node('ur5_joint_estimator')
 r = rospy.Rate(1)
@@ -29,3 +32,8 @@ while not rospy.is_shutdown():
 	r.sleep()
 	a.estimateJointValueByUsingTF()
 	print
+
+
+
+# 	Translation: [0.365, 0.173, 0.513]
+# - Rotation: in Quaternion [0.707, 0.707, -0.001, -0.001]
