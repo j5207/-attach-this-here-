5a2268f482cd923c88fb8e84e71a51039e0686e3
ZedLee97 <33314876+ZedLee97@users.noreply.github.com>
2018-08-16 03:55:20 +0800

    Add files via upload

--- hand_tracking_node.py
+++ hand_tracking_node.py
@@ -17,7 +17,7 @@ voice_flag = 0
 
 def warp_img(img):
     #pts1 = np.float32([[115,124],[520,112],[2,476],[640,480]])
-    pts1 = np.float32([[101,160],[531,133],[0,480],[640,480]])
+    pts1 = np.float32([[268,76],[500,58],[272,252],[523,237]])
     pts2 = np.float32([[0,0],[640,0],[0,480],[640,480]])
     M = cv2.getPerspectiveTransform(pts1,pts2)
     dst = cv2.warpPerspective(img,M,(640,480))
@@ -49,9 +49,9 @@ def get_objectmask(img):
     hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     green_mask = cv2.inRange(hsv, Green_low, Green_high)
     hand_mask = cv2.inRange(hsv, Hand_low, Hand_high)
-    hand_mask = cv2.dilate(hand_mask, kernel = np.ones((7,7),np.uint8))
+    hand_mask = cv2.dilate(hand_mask, kernel = np.ones((11,11),np.uint8))
     skin_mask = cv2.inRange(hsv, Skin_low, Skin_high)
-    skin_mask = cv2.dilate(skin_mask, kernel = np.ones((7,7),np.uint8))
+    skin_mask = cv2.dilate(skin_mask, kernel = np.ones((11,11),np.uint8))
     thresh = 255 - green_mask
     thresh = cv2.subtract(thresh, hand_mask)
     thresh = cv2.subtract(thresh, skin_mask)
@@ -67,6 +67,7 @@ class temp_tracking():
         self.cap = cv2.VideoCapture(0)
 
     def update(self):
+        self.surfacels = []
         self.boxls = []
         OK, origin = self.cap.read()
         x = None
@@ -74,19 +75,26 @@ class temp_tracking():
             rect = camrectify(origin)
             warp = warp_img(rect)
             thresh = get_objectmask(warp)
+            cv2.imshow('thresh', thresh)
+            self.image = warp.copy()
             draw_img1 = warp.copy()
             self.get_bound(draw_img1, thresh, visualization=True)
-            
+            cx, cy = None, None
+            #print(self.surfacels)
             (point, angle), (point2, angle2), center = hand_tracking(warp_img(origin), cache(10), cache(10)).get_result()
             if point and len(self.boxls) > 0:
                 length_ls = []
                 for x, y, w, h in self.boxls:
                     length_ls.append((get_k_dis((point[0], point[1]), (center[0], center[1]), (x+w/2, y+h/2)), (x+w/2, y+h/2)))
-                x,y = min(length_ls, key=lambda x: x[0])[1]
-                ind = test_insdie((x, y), self.boxls)
-                x, y, w, h = self.boxls[ind]
-                cv2.rectangle(draw_img1,(x,y),(x+w,y+h),(0,0,255),2)
-                cv2.putText(draw_img1,"pointed",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0,(0,0,255))
+                length_ls = filter(lambda x: (point[1] - x[1][1]) * (point[1] - center[1]) <= 0, length_ls)
+                if len(length_ls) > 0:
+                    x,y = min(length_ls, key=lambda x: x[0])[1]
+                    ind = test_insdie((x, y), self.boxls)
+                    x, y, w, h = self.boxls[ind]
+                    cx, cy = self.surfacels[ind]
+                    cv2.rectangle(draw_img1,(x,y),(x+w,y+h),(0,0,255),2)
+                    cv2.circle(draw_img1, (cx, cy), 5, (0, 0, 255), -1)
+                    cv2.putText(draw_img1,"pointed",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0,(0,0,255))
             
             if point2 and len(self.boxls) > 0:
                 boxls = deepcopy(self.boxls)
@@ -101,25 +109,29 @@ class temp_tracking():
                 del boxls[rind]
                 cv2.rectangle(draw_img1,(x,y),(x+w,y+h),(0,0,255),2)
                 cv2.putText(draw_img1,"pointed_right",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0,(0,0,255))
-
-                for x, y, w, h in boxls:
-                    length_lsl.append((get_k_dis((lpoint[0], lpoint[1]), (center[0], center[1]), (x+w/2, y+h/2)), (x+w/2, y+h/2)))
-                lx,ly = min(length_lsl, key=lambda x: x[0])[1]
-                lind = test_insdie((lx, ly), boxls)
-                x, y, w, h = boxls[lind]
-                cv2.rectangle(draw_img1,(x,y),(x+w,y+h),(0,0,255),2)
-                cv2.putText(draw_img1,"pointed_left",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0,(0,0,255))
+                if len(boxls) > 0:
+                    for x, y, w, h in boxls:
+                        length_lsl.append((get_k_dis((lpoint[0], lpoint[1]), (center[0], center[1]), (x+w/2, y+h/2)), (x+w/2, y+h/2)))
+                    lx,ly = min(length_lsl, key=lambda x: x[0])[1]
+                    lind = test_insdie((lx, ly), boxls)
+                    x, y, w, h = boxls[lind]
+                    cv2.rectangle(draw_img1,(x,y),(x+w,y+h),(0,0,255),2)
+                    cv2.putText(draw_img1,"pointed_left",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0,(0,0,255))
         
         cv2.imshow('draw', draw_img1)
-        if x and point:
-            return [[point[0],point[1]],[x+w/2,y+h/2]]
+        if cx and point:
+            return [[point[0],point[1]],[cx,cy]]
+        if point2 and lx and rx:
+            return [[point2[0][0], point2[0][1]], [point2[1][0], point2[1][1]], [rx, ry], [lx, ly]]
+        # if x and point:
+        #     return [[point[0],point[1]],[x+w/2,y+h/2]]
 
     def get_bound(self, img, object_mask, visualization=True):
         (_,object_contours, object_hierarchy)=cv2.findContours(object_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         if len(object_contours) > 0:
             for i , contour in enumerate(object_contours):
                 area = cv2.contourArea(contour)
-                if area>100 and area < 100000 and object_hierarchy[0, i, 3] == -1:					
+                if area>200 and area < 2000 and object_hierarchy[0, i, 3] == -1:					
                     M = cv2.moments(contour)
                     cx = int(M['m10']/M['m00'])
                     cy = int(M['m01']/M['m00'])
@@ -128,7 +140,20 @@ class temp_tracking():
         if len(self.boxls) > 0:
             boxls_arr = np.array(self.boxls)
             self.boxls = boxls_arr[boxls_arr[:, 0].argsort()].tolist()
-    
+        for x, y, w, h in self.boxls:
+            sub = self.image[y:y+h, x:x+w, :]
+            hsv = cv2.cvtColor(sub,cv2.COLOR_BGR2HSV)
+            top_mask = cv2.inRange(hsv, Top_low, Top_high)
+            (_,top_contours, object_hierarchy)=cv2.findContours(top_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            max_area = 0
+            for i , contour in enumerate(top_contours):
+                area = cv2.contourArea(contour)
+                if area>max_area and object_hierarchy[0, i, 3] == -1:					
+                    M = cv2.moments(contour)
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+                    max_area = area
+            self.surfacels.append((cx+x, cy+y))
 
     def __del__(self):
         self.cap.release()
@@ -150,10 +175,20 @@ if __name__ == '__main__':
         pos = temp.update()
         if pos:
             if voice_flag == 1 and not ready_for_place:
-                pos_N_cmd.append([pos[1][0], pos[1][1]])
+                if len(pos) == 2:
+                    pos_N_cmd.append(int(pos[1][0]))
+                    pos_N_cmd.append(int(pos[1][1]))
+                elif len(pos) == 4:
+                    pos_N_cmd.append(int(pos[2][0]))
+                    pos_N_cmd.append(int(pos[2][1]))
+                    pos_N_cmd.append(int(pos[3][0]))
+                    pos_N_cmd.append(int(pos[3][1]))
+                print(pos_N_cmd)
                 ready_for_place = True
             elif voice_flag == 2 and ready_for_place:
-                pos_N_cmd.append([pos[0][0], pos[0][1]])
+                pos_N_cmd.append(int(pos[0][0]))
+                pos_N_cmd.append(int(pos[0][1]))
+                print(pos_N_cmd)
                 pub.publish(Int32MultiArray(data=pos_N_cmd))
                 ready_for_place = False
                 pos_N_cmd = []
@@ -163,3 +198,4 @@ if __name__ == '__main__':
         if k == 113 or k == 27:
             break
     cv2.destroyAllWindows()
+
