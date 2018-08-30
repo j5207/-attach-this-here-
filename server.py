c1f88bc64fdbbee753ad01dc517ce8b637911c65
ZedLee97 <33314876+ZedLee97@users.noreply.github.com>
2018-08-30 21:50:01 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -19,13 +19,13 @@ def draw_x(img, x, y, line):
     return img
 
 def draw_arrow(img, x1, y1, x2, y2, line):
-    cv2.arrowedLine(img, (int(0.8*x1 + 0.2*x2), int(0.8*y1 + 0.2*y2)), (int(0.2*x1 + 0.8*x2), int(0.2*y1+0.8*y2)),(255, 255, 255), line)
+    cv2.arrowedLine(img, (int(0.6*x1 + 0.4*x2), int(0.6*y1 + 0.4*y2)), (int(0.4*x1 + 0.6*x2), int(0.4*y1+0.6*y2)),(255, 255, 255), line)
     return img
 
 img = np.zeros((900, 1600, 3), np.uint8) * 255
-cv2.circle(img, (int(1600*428/640),int(207*900/480)), 40, (255, 255, 255), -1)
+cv2.circle(img, (int(1600*428/640) - 50,int(207*900/480) + 60), 40, (255, 255, 255), -1)
 img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
-img = draw_arrow(img, int(1600*325/640), int(420*900/480), int(1600*207/640),int(207*900/480),5)
+img = draw_arrow(img, int(1600*428/640) - 20, int(207*900/480) + 35, int(1600*325/640),int(420*900/480),5)
 cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow("projector", img)
@@ -34,27 +34,27 @@ cv2.waitKey(0)
 
 img = np.zeros((900, 1600, 3), np.uint8) * 255
 cv2.circle(img, (int(1600*428/640),int(207*900/480)), 40, (255, 255, 255), -1)
-cv2.circle(img, (int(1600*428/640+80),int(207*900/480+80)), 40, (255, 255, 255), -1)
-img = draw_x(img,int(207*1600/640),int(207*900/480), 40)
-img = draw_arrow(img, int(1600*428/640+40), int(207*900/480+40), int(1600*207/640),int(207*900/480),5)
+cv2.circle(img, (int(1600*428/640+60),int(207*900/480+60)), 40, (255, 255, 255), -1)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*428/640+40), int(207*900/480+40), int(1600*325/640),int(420*900/480),5)
 cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow("projector", img)
 cv2.waitKey(0)
 
 img = np.zeros((900, 1600, 3), np.uint8) * 255
-cv2.circle(img, (int(1600*428/640),int(207*900/480)), 100, (255, 255, 255), 5)
-img = draw_x(img,int(207*1600/640),int(207*900/480), 40)
-img = draw_arrow(img, int(1600*428/640), int(207*900/480), int(1600*207/640),int(207*900/480),5)
+cv2.circle(img, (int(1600*428/640 -10),int(207*900/480 + 20)), 120, (255, 255, 255), 5)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*428/640 + 20), int(207*900/480 + 20), int(1600*325/640),int(420*900/480),5)
 cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow("projector", img)
 cv2.waitKey(0)
 
 img = np.zeros((900, 1600, 3), np.uint8) * 255
-cv2.circle(img, (int(1600*200/640),int(400*900/480)), 70, (255, 255, 255), 5)
-img = draw_x(img,int(207*1600/640),int(207*900/480), 40)
-img = draw_arrow(img, int(1600*200/640), int(400*900/480), int(1600*207/640),int(207*900/480),5)
+cv2.circle(img, (int(1600*200/640),int(400*900/480)), 120, (255, 255, 255), 5)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*200/640), int(400*900/480), int(1600*325/640),int(420*900/480),5)
 cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow("projector", img)
@@ -63,18 +63,140 @@ cv2.waitKey(0)
 img = np.zeros((900, 1600, 3), np.uint8) * 255
 cv2.circle(img, (int(1600*500/640),int(400*900/480)), 40, (255, 255, 255), -1)
 cv2.circle(img, (int(1600*500/640+80),int(400*900/480)), 40, (255, 255, 255), -1)
-img = draw_x(img,int(207*1600/640),int(207*900/480), 40)
-img = draw_arrow(img, int(1600*500/640+40), int(400*900/480), int(1600*207/640),int(207*900/480),5)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*500/640+40), int(400*900/480), int(1600*325/640),int(420*900/480),5)
+cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("projector", img)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+img = np.zeros((900, 1600, 3), np.uint8) * 255
+cv2.circle(img, (int(1600*500/640),int(400*900/480)), 40, (255, 255, 255), -1)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*500/640), int(400*900/480), int(1600*325/640),int(420*900/480),5)
+cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("projector", img)
+cv2.waitKey(0)
+
+
+img = np.zeros((900, 1600, 3), np.uint8) * 255
+cv2.circle(img, (int(1600*200/640),int(400*900/480)), 40, (255, 255, 255), -1)
+cv2.circle(img, (int(1600*200/640+100),int(400*900/480)), 40, (255, 255, 255), -1)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*200/640+30), int(400*900/480+30), int(1600*325/640),int(420*900/480),5)
 cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow("projector", img)
 cv2.waitKey(0)
 
 img = np.zeros((900, 1600, 3), np.uint8) * 255
+cv2.circle(img, (int(1600*275/640 +50),int(190*900/480)), 120, (255, 255, 255), 5)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*275/640 + 50), int(190*900/480 + 20), int(1600*325/640),int(420*900/480),5)
 cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow("projector", img)
 cv2.waitKey(0)
+
+img = np.zeros((900, 1600, 3), np.uint8) * 255
+cv2.circle(img, (int(1600*500/640 + 40),int(400*900/480)), 120, (255, 255, 255), 5)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*500/640 + 40), int(400*900/480), int(1600*325/640),int(420*900/480),5)
+cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("projector", img)
+cv2.waitKey(0)
+
+img = np.zeros((900, 1600, 3), np.uint8) * 255
+cv2.circle(img, (int(1600*275/640),int(190*900/480)), 40, (255, 255, 255), -1)
+cv2.circle(img, (int(1600*275/640+80),int(190*900/480)), 40, (255, 255, 255), -1)
+img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+img = draw_arrow(img, int(1600*275/640+40), int(190*900/480), int(1600*325/640),int(420*900/480),5)
+cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("projector", img)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+img = np.zeros((900, 1600, 3), np.uint8) * 255
+cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("projector", img)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+# img = np.zeros((900, 1600, 3), np.uint8) * 255
+# cv2.circle(img, (int(1600*150/640),int(300*900/480)), 40, (255, 255, 255), -1)
+# img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+# img = draw_arrow(img, int(1600*428/640), int(207*900/480), int(1600*325/640),int(420*900/480),5)
+# cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# cv2.imshow("projector", img)
+# cv2.waitKey(0)
+#
+#
+# img = np.zeros((900, 1600, 3), np.uint8) * 255
+# cv2.circle(img, (int(1600*150/640),int(300*900/480)), 40, (255, 255, 255), -1)
+# cv2.circle(img, (int(1600*150/640+60),int(300*900/480+60)), 40, (255, 255, 255), -1)
+# img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+# img = draw_arrow(img, int(1600*428/640+40), int(207*900/480+40), int(1600*325/640),int(420*900/480),5)
+# cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# cv2.imshow("projector", img)
+# cv2.waitKey(0)
+#
+# img = np.zeros((900, 1600, 3), np.uint8) * 255
+# cv2.circle(img, (int(1600*150/640),int(300*900/480)), 80, (255, 255, 255), 5)
+# img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+# img = draw_arrow(img, int(1600*428/640), int(207*900/480), int(1600*325/640),int(420*900/480),5)
+# cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# cv2.imshow("projector", img)
+# cv2.waitKey(0)
+#
+# img = np.zeros((900, 1600, 3), np.uint8) * 255
+# cv2.circle(img, (int(1600*200/640),int(400*900/480)), 60, (255, 255, 255), 5)
+# img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+# img = draw_arrow(img, int(1600*200/640), int(400*900/480), int(1600*325/640),int(420*900/480),5)
+# cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# cv2.imshow("projector", img)
+# cv2.waitKey(0)
+#
+# img = np.zeros((900, 1600, 3), np.uint8) * 255
+# cv2.circle(img, (int(1600*500/640),int(400*900/480)), 40, (255, 255, 255), -1)
+# cv2.circle(img, (int(1600*500/640+80),int(400*900/480)), 40, (255, 255, 255), -1)
+# img = draw_x(img,int(325*1600/640),int(420*900/480), 40)
+# img = draw_arrow(img, int(1600*500/640+40), int(400*900/480), int(1600*325/640),int(420*900/480),5)
+# cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# cv2.imshow("projector", img)
+# cv2.waitKey(0)
+#
+# img = np.zeros((900, 1600, 3), np.uint8) * 255
+# cv2.namedWindow("projector", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("projector", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+# cv2.imshow("projector", img)
+# cv2.waitKey(0)
+
 
 
 # server = socket.socket()    # 默认是AF_INET、SOCK_STREAM
