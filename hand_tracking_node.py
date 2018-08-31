942baaf76fe44d2537f71742194f0a88f81c8fc9
Ji Han <jhan53@jhu.edu>
2018-08-31 10:10:52 -0400

    update

--- hand_tracking_node.py
+++ hand_tracking_node.py
@@ -472,8 +472,9 @@ class temp_tracking():
                         length_ls.append((get_k_dis((point[0], point[1]), (center[0], center[1]), (x+w/2, y+h/2)), (x+w/2, y+h/2)))
                     length_ls = filter(lambda x: (point[1] - x[1][1]) * (point[1] - center[1]) <= 0, length_ls)
                     length_ls = filter(lambda x: (point[0] - x[1][0]) * (center[0] - x[1][0]) > 0, length_ls)
+                    length_ls = filter(lambda x: x[1][1] - point[1] < 0, length_ls)
                     #print("haha", len(length_ls))
-                    length_ls = filter(lambda x: x[0] < 50, length_ls)
+                    length_ls = filter(lambda x: x[0] < 40, length_ls)
                     #print("ddd", len(length_ls))
                     sub_result = []
                     if len(length_ls) > 0:
